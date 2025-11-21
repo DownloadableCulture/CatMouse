4502c86db2c75b05410392f45683329555017a56
Jelle Gemert <jeleeinfo@gmail.com>
2025-11-21 18:42:00 +0100

    merged scroll and adjust x into one method since the should always happen at once

--- Assets/Scroller.cs
+++ Assets/Scroller.cs
@@ -13,13 +13,20 @@ public class TrackScroller : MonoBehaviour
         {
             Scroll(child);
             ResetTrack(child);
-            AdjustX(child);
+    
         }
     }
 
     void Scroll(Transform t)
     {
+        float yStart = 5f;
+        float xStart = 2f;
+        float slope = -0.2f;
+
         t.position += Vector3.down * speed * Time.deltaTime;
+
+        float x = xStart + (t.position.y - yStart) * slope;
+        t.position = new Vector3(x, t.position.y, t.position.z);
     }
 
     void ResetTrack(Transform t)
@@ -29,14 +36,6 @@ public class TrackScroller : MonoBehaviour
             t.position = new Vector3(t.position.x, startPosition, t.position.z);
         }
     }
-    void AdjustX(Transform child)
-    {
-        float yStart = 5f;
-        float xStart = 2f;
-        float slope = -0.2f;
 
-        float x = xStart + (child.position.y - yStart) * slope;
-        child.position = new Vector3(x, child.position.y, child.position.z);
-    }
 
 }
