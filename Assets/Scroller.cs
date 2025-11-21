c41115154658eda15f631b0bfe08933aef6039bf
Jelle Gemert <jeleeinfo@gmail.com>
2025-11-21 20:31:33 +0100

    functional x axis movement scaling

--- Assets/Scroller.cs
+++ Assets/Scroller.cs
@@ -4,38 +4,45 @@ public class TrackScroller : MonoBehaviour
 {
     [Range(0f, 20f)]
     [SerializeField] float speed = 3f;
-    public float endPosition = -10;
-    public float startPosition = 10;
+
+    public float startPosition = 10f;
+    public float endPosition = -10f;
+
+    float[] originalX;
+
+    void Start()
+    {
+        int count = transform.childCount;
+        originalX = new float[count];
+
+        for (int i = 0; i < count; i++)
+            originalX[i] = transform.GetChild(i).position.x;
+    }
 
     void Update()
     {
-        foreach (Transform child in transform)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            Scroll(child);
-            ResetTrack(child);
-    
+            ScrollChild(transform.GetChild(i), i);
         }
     }
 
-    void Scroll(Transform t)
+    void ScrollChild(Transform child, int index)
     {
-        float yStart = 5f;
-        float xStart = 2f;
-        float slope = -0.2f;
+        child.position += Vector3.down * speed * Time.deltaTime;
 
-        t.position += Vector3.down * speed * Time.deltaTime;
+        if (child.position.y <= endPosition)
+            child.position = new Vector3(originalX[index], startPosition, child.position.z);
 
-        float x = xStart + (t.position.y - yStart) * slope;
-        t.position = new Vector3(x, t.position.y, t.position.z);
-    }
+        float yTop = 5f;
+        float yBottom = -5f;
 
-    void ResetTrack(Transform t)
-    {
-        if (t.position.y <= endPosition)
+        if (child.position.y <= yTop && child.position.y >= yBottom)
         {
-            t.position = new Vector3(t.position.x, startPosition, t.position.z);
+            float t = (child.position.y - yTop) / (yBottom - yTop);
+            float factor = 1f + t; 
+            float newX = originalX[index] * factor;
+            child.position = new Vector3(newX, child.position.y, child.position.z);
         }
     }
-
-
 }
